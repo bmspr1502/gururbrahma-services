95a5aee9c64d3167c86e2c258c07f94ee872b65a
Pranava Raman BMS <pranavaraman.bms@gmail.com>
2025-12-23 15:20:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5159116962-cf239",
+  "appId": "1:296728420940:web:081fcfb021c1ba6ea94ae9",
+  "apiKey": "AIzaSyDWLvQv6tXVuA-nGminpP0l7LetUrkTohg",
+  "authDomain": "studio-5159116962-cf239.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "296728420940"
+};
